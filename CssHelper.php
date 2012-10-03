4ba9b34129d923569014b0222f1c4bd0f912f8eb
Gabe Wells <gmwells91@gmail.com>
2012-10-02 21:02:32 -0400

    added comment
    explaining methodology

--- CssHelper.php
+++ CssHelper.php
@@ -1,10 +1,17 @@
 <?php
 
+/***
+hook predispatch to inject css
+tried predispatch hooking on a zend_translate adapter
+object, it works!
+***/
+
 class Css_Helper
 {
    protected $_version = "1.0";
    
    public function changePositionById($id)
+   
    {
    }
 }
