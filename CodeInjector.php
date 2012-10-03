cb95c0b441b4a80e61a9c90abac817177c9e9823
Gabe Wells <gmwells91@gmail.com>
2012-10-02 21:34:45 -0400

    add
    add

---
+++ CodeInjector.php
@@ -0,0 +1,22 @@
+<?php
+namespace Zend\Mvc\Controller\Plugin;
+
+use Zend\Http\Response;
+use Zend\Mvc\Exception;
+use Zend\Mvc\InjectApplicationEventInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteStackInterface;
+
+  
+use Zend\View\Model\ModelInterface as Model;
+
+/**
+ * @todo       allows positioning of elements to be offsetted
+ * @category   Zend
+ * @package    Zend_Mvc
+ * @subpackage Controller
+ */
+class CodeInjector extends AbstractPlugin
+{
+   	
+}
